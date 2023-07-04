5b28d5b15bd22ea13e334a6551014f7a5ecaa9d0
Hércules Lopes <69056516+herculeslopes@users.noreply.github.com>
2023-07-04 12:43:44 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,27 @@ import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    {enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */)},
-    react()
-  ],
+// export default defineConfig({
+//   plugins: [
+//     {enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */)},
+//     react()
+//   ],
+// })
+
+export default defineConfig(() => {
+  const config = {
+    plugins: [
+      { enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */) },
+      react()
+    ],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
 })
+
+// if (command !== 'serve') {
+//   config.base = '/react-vite-gh-pages/'
+// }
