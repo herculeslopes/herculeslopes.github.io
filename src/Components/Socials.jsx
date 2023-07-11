386bba2261ca7bf71850cfb5e0f278baf2cb3525
Hércules Lopes <69056516+herculeslopes@users.noreply.github.com>
2023-07-11 11:06:56 -0300

    update

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -1,15 +1,23 @@
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const Socials = () => {
   return <>
     <div className="socials">
       <div className="links">
+        <a href="#">
+          <FaEnvelope />
+          <div>
+            <span>herculeslopesti@gmail.com</span>
+          </div>
+        </a>
+
         <a href="https://github.com/herculeslopes" target='blank'>
           <FaGithub />
           <div>
             <span>https://github.com/herculeslopes</span>
           </div>
         </a>
+
         <a href="https://linkedin.com/in/herculeslopes" target='blank'>
           <FaLinkedin />
           <div>
